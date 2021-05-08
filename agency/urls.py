c07d9abbf74aa7fbed1393e2e305f6877ee5df08
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2021-05-08 18:53:14 +0900

    urlsに問い合わせページ(inquiry.htmlへのパスを追加)

--- agency/urls.py
+++ agency/urls.py
@@ -5,4 +5,6 @@ from.import views
 app_name = 'agency'
 urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
+    # 問い合わせページinquiry.htmlへのパス
+    path('inquiry',views.InquiryView.as_view(),name="inquiry"),
 ]
