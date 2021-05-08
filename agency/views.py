105dd7608597164c0cbc3e345833a9b2b92d88f9
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2021-05-08 19:16:37 +0900

    views.pyの編集

--- agency/views.py
+++ agency/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+# genericの読み込みで使用できるようにする
 from django.views import generic
-
+# 問い合わせページInquiryFormの読み込み
+from.forms import InquiryForm
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 # 自分で定義したIndexViewに引数でgenericのTenplateViewクラスを渡している。
 # as_view()メソッドはTemplateViewクラス内で定義されている。
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
