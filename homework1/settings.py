52b707a94c992281a6ea3676d9032e708d484756
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2021-06-06 09:48:38 +0900

    djangoのデータベース設定をPostgreSQLに変更

--- homework1/settings.py
+++ homework1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'homework1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework1',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
 }
 
